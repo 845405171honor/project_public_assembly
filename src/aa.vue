e2c8ba9991137c37bca07767c3f794cde4243d78
apply-boy <845405171@qq.com>
2022-07-11 00:42:27 +0800

    file commit

---
+++ src/aa.vue
@@ -0,0 +1,38 @@
+<template>
+   <div class="slotClass">
+      <div>
+          111
+          <slot name="aaClass" v-for="(item, index) in table" :aa="item"></slot>
+          333
+      </div>
+   </div>
+</template>
+
+<script>
+
+export default {
+    name: 'edit-list-lab',
+    props:{
+        table: {
+            type: Array,
+            default: function(){return []}
+        }
+    },
+    created() {
+        this.$nextTick(()=>{
+            console.log('table', this.table)
+        });
+    },
+    data(){
+        return {
+        }
+    },
+
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+</style>
